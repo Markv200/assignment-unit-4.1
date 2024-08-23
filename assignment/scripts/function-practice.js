332bbb56dee79995a8674f9d4c509490e2fe09df
Mark Velesaca <mark.velesaca@gmail.com>
2024-08-23 03:52:48 -0500

    Everything

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -10,36 +10,37 @@ function hello() {
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
-
-
+console.log(hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
-
+console.log(helloName('Mark'));
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber,secondNumber) {
+  return firstNumber + secondNumber;
   // return firstNumber + secondNumber;
 }
-
-
+console.log(addNumbers(5,4));
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(num1, num2, num3) {
+  return num1 * num2 * num3;
 }
-
+console.log(multiplyThree(5,3,2));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+    return false;
 }
+
+console.log(isPositive(5));
+console.log(isPositive(-1));
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 
@@ -47,16 +48,28 @@ function isPositive(number) {
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+if (array.length === 0){
+  return undefined;
 }
+return array[array.length - 1];
+}
+
+console.log(getLast ([]));
+console.log(getLast([1,2,3]))
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+  for (let i=0; i<array.length; i++){
+    if (array[i] === value)
+      return true;
 }
-
+      return false;
+}
+console.log(find(3, [1,2,3,4]));
+console.log(find(23, [1,2,3,4,5]));
 // ----------------------
 // Stretch Goals
 // ----------------------
